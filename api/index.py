ffa8bb21a5cc86291b33adef01875397e6a3e756
Hargaaya Idris <hargaaya10@gmail.com>
2023-07-30 12:33:06 +0200

    add token check

--- api/index.py
+++ api/index.py
@@ -1,7 +1,17 @@
-from flask import Flask
+import sys
+from utils import verify_token
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+@app.before_request
+def check_token_callback():
+    try:
+        verify_token()
+        
+    except(Exception):
+        return jsonify(error = "missing a valid token"), 401
+
 @app.route('/')
 def parser():
-    return "Hello World!"
+    return jsonify(data = "Hello World!")
